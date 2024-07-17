f9343af9d39ad391ce34717f01a4b1b3d8ea517e
Javmir <167805975+javgamma@users.noreply.github.com>
2024-07-17 19:24:43 +0200

    none

--- src/components/ComponentC.jsx
+++ src/components/ComponentC.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ComponentA from './ComponentA'
 
 const ComponentC = ({x, incrementX}) => {
+  
 
   return (
     <div className='border bg-white-200 w-[300px] text-left mr-2 p-4'>
@@ -11,6 +12,8 @@ const ComponentC = ({x, incrementX}) => {
       onClick={incrementX}
       className='bg-blue-500 text-white px-4 py-2 rounded mt-2 border'>x++</button>
     </div>
+
+
   )
 }
 
